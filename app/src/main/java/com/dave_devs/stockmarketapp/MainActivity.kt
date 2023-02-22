fa17ff8203cbc8d5d4cabc1fb887c92db4a0891e
Oreoluwa Odetunde <102271576+LSR07@users.noreply.github.com>
2023-02-22 23:23:50 +0100

    first commit 0.3

--- app/src/main/java/com/dave_devs/stockmarketapp/MainActivity.kt
+++ app/src/main/java/com/dave_devs/stockmarketapp/MainActivity.kt
@@ -3,14 +3,27 @@ package com.dave_devs.stockmarketapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.dave_devs.stockmarketapp.feature_stock.presentation.company_listings.CompanyListingsScreen
 import com.dave_devs.stockmarketapp.ui.theme.StockMarketAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             StockMarketAppTheme {
-
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CompanyListingsScreen()
+                    //DestinationsNavHost(navGraph = )
+                }
             }
         }
     }
--- app/src/main/java/com/dave_devs/stockmarketapp/feature_stock/presentation/company_listings/CompanyListingsScreen.kt
+++ app/src/main/java/com/dave_devs/stockmarketapp/feature_stock/presentation/company_listings/CompanyListingsScreen.kt
@@ -1,16 +1,90 @@
 package com.dave_devs.stockmarketapp.feature_stock.presentation.company_listings
 
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.RootNavGraph
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-@Destination(start = true)
+@RootNavGraph(start = true)
+@Destination
 fun CompanyListingsScreen(
     modifier: Modifier = Modifier,
+    //Navigator: DestinationsNavigator,
     viewModel: CompanyListingsViewModel = hiltViewModel()
 ) {
+    val swipeRefreshState = rememberSwipeRefreshState(
+        isRefreshing = viewModel.state.isRefreshing
+    )
+    val state = viewModel.state
 
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        OutlinedTextField(
+            value = state.searchQuery,
+            onValueChange = {
+                viewModel.onEvent(
+                    CompanyListingsEvent.OnSearchQueryChange(it)
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            placeholder = {
+                Text(
+                    text = "Search..."
+                )
+            },
+            maxLines = 1,
+            singleLine = true,
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                textColor = MaterialTheme.colorScheme.primary,
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                placeholderColor = MaterialTheme.colorScheme.primaryContainer
+            )
+        )
+        SwipeRefresh(
+            state = swipeRefreshState,
+            onRefresh = {
+                viewModel.onEvent(
+                    CompanyListingsEvent.Refresh
+                )
+            }
+        ) {
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(state.companies.size) { i ->
+                    val company = state.companies[i]
+                    CompanyListingItem(
+                        company = company,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                //TODO//
+                            }
+                            .padding(16.dp)
+                    )
+                    if(i < state.companies.size) {
+                        Divider(
+                            modifier = Modifier.padding(horizontal = 16.dp)
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
